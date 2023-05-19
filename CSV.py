454a000af740fb8c05c356c476c40b1825b52e39
ChMHuzaifa <134031703+ChMHuzaifa@users.noreply.github.com>
2023-05-19 09:18:51 +0500

    Update CSV.py

--- CSV.py
+++ CSV.py
@@ -1,1 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load data from a CSV file
+data = pd.read_csv('data.csv')
+
+# Split the data into features (X) and target variable (y)
+X = data[['feature1', 'feature2', 'feature3']]
+y = data['target']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Calculate the mean squared error
+mse = mean_squared_error(y_test, predictions)
+
+# Print the mean squared error
+print("Mean Squared Error:", mse)
 
